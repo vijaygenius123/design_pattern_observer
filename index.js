425655c5263be0d78f9049ed1b4e1d16aaff11d8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-13 19:20:37 +0530

    Implemented Observer Pattern

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
 var Sensor = /** @class */ (function () {
     function Sensor() {
+        this.observers = [];
     }
     Sensor.prototype.setTemperature = function (temp) {
         console.log("Got Temperature : ", temp);
         this.temperature = temp;
+        this.notifyObservers();
+    };
+    Sensor.prototype.registerObserver = function (o) {
+        this.observers.push(o);
+    };
+    Sensor.prototype.unregisterObserver = function (o) {
+        var index = this.observers.indexOf(o);
+        this.observers.splice(index, 1);
+    };
+    Sensor.prototype.notifyObservers = function () {
+        for (var _i = 0, _a = this.observers; _i < _a.length; _i++) {
+            var observer = _a[_i];
+            observer.update(this.temperature);
+        }
     };
     return Sensor;
 }());
 var Fan = /** @class */ (function () {
-    function Fan() {
+    function Fan(sensor) {
+        this.subject = sensor;
+        var fan_list = document.querySelector('#fan_list');
+        var li = document.createElement('li');
+        li.innerText = "New Fan";
+        this.node = li;
+        fan_list.appendChild(li);
+        sensor.registerObserver(this);
     }
+    Fan.prototype.update = function (temp) {
+        console.log("Got Temperature Update ", temp);
+        if (temp > 30) {
+            this.node.classList.remove('cold');
+            this.node.classList.add('hot');
+        }
+        else if (temp < 20) {
+            this.node.classList.remove('hot');
+            this.node.classList.add('cold');
+        }
+        else {
+            this.node.classList.remove('hot');
+            this.node.classList.remove('cold');
+        }
+        this.node.innerText = 'Temperature : ' + temp;
+    };
     return Fan;
 }());
 var sensor = new Sensor();
-//sensor.setTemperature(35);
+var fan = new Fan(sensor);
 var temperature_slider = document.querySelector('#temperature_slider');
 temperature_slider.addEventListener('change', function (evt) {
     sensor.setTemperature(evt.target.value);
--- index.ts
+++ index.ts
@@ -1,18 +1,78 @@
 
-class Sensor{
+interface Subject{
+
+    registerObserver(o: Observer);
+    unregisterObserver(o: Observer);
+    notifyObservers(temp: number);
+}
+
+interface Observer{
+    update(temp: number);
+}
+
+class Sensor implements Subject{
     private temperature: number;
+    private observers: Observer[] = [];
 
     setTemperature(temp: number){
         console.log("Got Temperature : ", temp);
         this.temperature = temp;
+        this.notifyObservers();
+    }
+
+    registerObserver(o: Observer) {
+        this.observers.push(o);
+    }
+
+    unregisterObserver(o: Observer) {
+        let index = this.observers.indexOf(o);
+        this.observers.splice(index, 1);
+    }
+
+    notifyObservers() {
+        for(let observer of this.observers){
+            observer.update(this.temperature);
+        }
     }
 }
 
-class Fan{
+class Fan implements Observer{
+
+    subject: Subject;
+    node: Node;
+
+    constructor(sensor: Subject) {
+        this.subject = sensor;
+
+        const fan_list = document.querySelector('#fan_list')
+        const li = document.createElement('li');
+        li.innerText = "New Fan"
+        this.node = li;
+        fan_list.appendChild(li);
+
+        sensor.registerObserver(this);
+    }
+
+    update(temp: number) {
+        console.log("Got Temperature Update ", temp);
+        if(temp > 30){
+            this.node.classList.remove('cold');
+            this.node.classList.add('hot');
+        }else if (temp < 20){
+            this.node.classList.remove('hot')
+            this.node.classList.add('cold');
+        }else{
+            this.node.classList.remove('hot')
+            this.node.classList.remove('cold')
+        }
+        this.node.innerText = 'Temperature : ' + temp;
+    }
+
+
 }
 
 let sensor = new Sensor();
-//sensor.setTemperature(35);
+let fan = new Fan(sensor);
 
 const temperature_slider = document.querySelector('#temperature_slider')
 temperature_slider.addEventListener('change', function (evt){
